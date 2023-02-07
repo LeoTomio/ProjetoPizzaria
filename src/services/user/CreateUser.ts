320d6aa7cd17695aabacc54e5efb1e1388272c54
leoTomio <leo__tomio@hotmail.com>
2023-02-07 09:35:19 -0300

    0.0.3 criando rota de inserção de users

--- src/services/user/CreateUser.ts
+++ src/services/user/CreateUser.ts
@@ -1,9 +1,33 @@
 import { UserRequest } from '../../interface/user/CreateUser'
+import prismaClient from '../../prisma'
 
 class CreateUserService {
     async execute(response: UserRequest) {
-        console.log(response.name);
-        return ({ ok: true })
+        const { name, email, password } = response
+        console.log(response)
+        //Verifica se foi enviado um email
+        if (!email) {
+            throw new Error("Email incorreto")
+        }
+
+        //Verifica se esse email já está cadastrado no banco
+        await prismaClient.user.findFirst({ where: { email: email } }).then(async (exists) => {
+            if (exists)
+                throw new Error('Usuario ja existe')
+        })
+
+        const user = await prismaClient.user.create({
+            data: {
+                ...response
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true
+            }
+        })
+
+        return user
     }
 }
 
